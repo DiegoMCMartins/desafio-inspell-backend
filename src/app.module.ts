8a133601eb5c9cc7101d29f65d7e29327f420993
Diego Miguel <diegomiguel2.8@hotmail.com>
2025-09-20 14:04:19 -0300

    feat: Inicio do modulo de controle do periodo de trabalho

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PeriodsModule } from './periods/periods.module';
 
 @Module({
-  imports: [],
+  imports: [PeriodsModule],
   controllers: [],
   providers: [],
 })
